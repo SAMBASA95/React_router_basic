306725d6a64e9a2a277ba2672c1a710cae21058b
swapnilukey1995 <83223911+swapnilukey1995@users.noreply.github.com>
2022-02-11 21:29:40 +0530

    Add files via upload

---
+++ Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Error = () => {
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/" className="button">
+        <span>
+          Back Home
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default Error;
---
+++ People.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { data } from '../../../data';
+import { Link } from 'react-router-dom';
+const People = () => {
+  const [people, setPeople] = useState(data);
+  return (
+    <div>
+      <h1>People Page</h1>
+      <div className="wrapper_items">
+        {people.map((person) => {
+          return (
+            <div key={person.id} className='item block'>
+              <h4>{person.name}</h4>
+              <Link className="block" to={`/person/${person.id}`}>
+                Learn More
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default People;
